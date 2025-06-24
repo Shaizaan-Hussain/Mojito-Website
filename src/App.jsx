e5e65d721a1f44e1b2d022eb79c5979ddc4fe6f8
Shaizaan Hussain <shaizaan.hussain786@gmail.com>
2025-06-24 17:22:40 +0530

    Implement the first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { ScrollTrigger, SplitText } from "gsap/all"
 import gsap from "gsap";
+import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
 
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div className="flex-center">
-            <h1 className="text-3xl text-indigo-600">App</h1>
-    </div>
+    <main>
+        <NavBar />
+        <Hero />
+        <div className="h-dvh bg-black" />
+    </main>
   )
 }
 
