839bababfd0098379f9d696a694e71a5efca2dad
Shaizaan Hussain <shaizaan.hussain786@gmail.com>
2025-06-24 19:56:36 +0530

    Update Home Section

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
  return (
 	<nav>
 	 <div className='pl-10 pr-10'>
-		<a href="#home" className="flex items-center gap-2">
+		<a href="#hero" className="flex items-center gap-2">
 		 <img src="/images/logo.png" alt="logo" />
 		 <p>Velvet Pour</p>
 		</a>
